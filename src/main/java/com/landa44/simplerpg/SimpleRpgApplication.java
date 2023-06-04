66474da8597528377cb983614d01f478b4b9fdab
landa44 <pablo44landa@gmail.com>
2023-06-04 07:17:04 -0400

    returning all heroes in get

--- src/main/java/com/landa44/simplerpg/SimpleRpgApplication.java
+++ src/main/java/com/landa44/simplerpg/SimpleRpgApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimpleRpgApplication {
+public class  SimpleRpgApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleRpgApplication.class, args);
