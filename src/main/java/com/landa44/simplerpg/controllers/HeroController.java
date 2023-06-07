13fd145f7646341c4e8247a529d246cdcf2f907b
landa44 <pablo44landa@gmail.com>
2023-06-06 22:13:03 -0400

    added seed for the db

--- src/main/java/com/landa44/simplerpg/controllers/HeroController.java
+++ src/main/java/com/landa44/simplerpg/controllers/HeroController.java
@@ -3,16 +3,14 @@ package com.landa44.simplerpg.controllers;
 import com.landa44.simplerpg.models.Hero;
 import com.landa44.simplerpg.services.HeroService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "rpg/v1/heroes")
-public class HeroController {
+public class  HeroController {
     private final HeroService heroService;
     @Autowired
     public HeroController(HeroService heroService) {
@@ -22,5 +20,10 @@ public class HeroController {
     @GetMapping
     public List<Hero> getHeroes(){
         return heroService.getHeroes();
+    }
+
+    @PostMapping
+    public void registerNewHero(@RequestBody Hero hero){
+        heroService.addNewHero(hero);
     }
 }
