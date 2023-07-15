951112c15c76dbe4088b8e191499fec68096a9dc
landa44 <pablo44landa@gmail.com>
2023-07-15 08:41:08 -0400

    applying Bruno's comments

--- src/main/java/com/landa44/simplerpg/SimpleRpgApplication.java
+++ src/main/java/com/landa44/simplerpg/SimpleRpgApplication.java
@@ -3,9 +3,7 @@ package com.landa44.simplerpg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * This is a Javadoc comment for the MyClass class.
- */
+@SuppressWarnings("checkstyle:MissingJavadocType")
 @SpringBootApplication
 public class  SimpleRpgApplication {
     public static void main(final String[] args) {
